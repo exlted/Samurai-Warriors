3f299dd94b74bb18f82e28a0d9dedca49d8da46a
exlted <exltedofdrg@gmail.com>
2016-01-07 15:06:20 -0500

    Fixing issues with API

---
+++ Solution1/Common_Console_Application1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace API
+{
+    public class Capi
+    {
+        public static string Read()//Shortcut for Console.ReadLine
+        {
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        public static char WaitForInput() //waits for any button press and returns pressed key, if key has no associated character, returns a instead
+        {
+            char input;
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            Console.WriteLine("Press Any Key to Continue");
+            try
+            {
+                input = Convert.ToChar(Console.ReadKey());
+                return input;
+            }
+            catch
+            {
+                return Convert.ToChar('a');
+            }
+        }
+    }
+}
