c917c6b9f8c78c907f2220d2eea35ab92c70efcb
exlted <exltedofdrg@gmail.com>
2016-01-07 14:39:25 -0500

    Initial Commit

---
+++ Solution1/Common_Console_Application1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using API;
+
+namespace Common_Console_Application1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Capi.WaitForInput();
+        }
+    }
+}
