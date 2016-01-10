16c001346d7ccbd5358ddea860eec91c2c0dd62d
exlted <exltedofdrg@gmail.com>
2016-01-09 20:32:17 -0500

    added default color

--- Solution1/Common_Console_Application1/World.cs
+++ Solution1/Common_Console_Application1/World.cs
@@ -7,7 +7,7 @@
         public char renderChar;
         public int colorCode;
 
-        public world(char render, bool passable, bool update, int color)
+        public world(char render, bool passable, bool update, int color = 1)
         {
             isPassable = passable;
             updateOnTick = update;
