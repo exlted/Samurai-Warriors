514a5cc77b11cc0e007b42f22ad7f3acf926b188
exlted <exltedofdrg@gmail.com>
2016-01-14 15:21:03 -0500

    Added arrow key support for menus

--- Solution1/Common_Console_Application1/Menu.cs
+++ Solution1/Common_Console_Application1/Menu.cs
@@ -16,8 +16,12 @@ namespace menu
             global.print(Console.WindowWidth / 2, 5, global.titleArt);
             for (int i = 0; i < menuItems.Length; i++)
             {
+                if (i + 1 == menuPos)
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+
                 global.print(Console.WindowWidth / 2 - 6, Console.WindowHeight / 2 + menuDrawPos, (i + 1) + ": "  + menuItems[i], false);
                 menuDrawPos += 2;
+                Console.BackgroundColor = ConsoleColor.Black;
             }
         }
         public static bool menu()
@@ -40,6 +44,28 @@ namespace menu
                         return true;
                     case ConsoleKey.NumPad2:
                         return false;
+                    case ConsoleKey.UpArrow:
+                        if (menuPos > 1)
+                        {
+                            menuPos -= 1;
+                            renderMenu(menuItems, menuPos);
+                            continue;
+                        }
+                        else
+                            continue;
+                    case ConsoleKey.DownArrow:
+                        if (menuPos < 2)
+                        {
+                            menuPos += 1;
+                            renderMenu(menuItems, menuPos);
+                            continue;
+                        }
+                        else
+                            continue;
+                    case ConsoleKey.Enter:
+                        if (menuPos == 1)
+                            return true;
+                        else return false;
                     default:
                         continue;
                 }
@@ -69,6 +95,31 @@ namespace menu
                         return true;
                     case ConsoleKey.NumPad2:
                         return false;
+                    case ConsoleKey.UpArrow:
+                        if (menuPos > 1)
+                        {
+                            menuPos -= 1;
+                            renderMenu(menuItems, menuPos);
+                            continue;
+                        }
+                        else
+                            continue;
+                    case ConsoleKey.DownArrow:
+                        if (menuPos < 2)
+                        {
+                            menuPos += 1;
+                            renderMenu(menuItems, menuPos);
+                            continue;
+                        }
+                        else
+                            continue;
+                    case ConsoleKey.Enter:
+                        if (menuPos == 1)
+                        {
+                            Render.initialRender();
+                            return true;
+                        }
+                        else return false;
                     default:
                         continue;
                 }
@@ -108,6 +159,39 @@ namespace menu
                         global.reInitPlayer();
                         global.world.Clear();
                         return false;
+                    case ConsoleKey.UpArrow:
+                        if (menuPos > 1)
+                        {
+                            menuPos -= 1;
+                            renderMenu(menuItems, menuPos);
+                            continue;
+                        }
+                        else
+                            continue;
+                    case ConsoleKey.DownArrow:
+                        if (menuPos < 2)
+                        {
+                            menuPos += 1;
+                            renderMenu(menuItems, menuPos);
+                            continue;
+                        }
+                        else
+                            continue;
+                    case ConsoleKey.Enter:
+                        if (menuPos == 1)
+                        {
+                            Render.initialRender(false);
+                            Render.initRender();
+                            Render.renderMobs();
+                            Render.renderUI();
+                            return true;
+                        }
+                        else
+                        {
+                            global.reInitPlayer();
+                            global.world.Clear();
+                            return false;
+                        }
                     default:
                         continue;
                 }
